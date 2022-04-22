19d944a0944de95d5fa7a51d95e9e5dd81056dbc
Alan Hou <houych@gmail.com>
2022-04-21 17:06:05 -0700

    Add support for L4T version 32.7.1

--- docker/configure.sh
+++ docker/configure.sh
@@ -15,6 +15,9 @@ then
 elif [[ "$L4T_VERSION" == "32.4.4" ]]
 then
 	JETBOT_BASE_IMAGE=nvcr.io/nvidia/l4t-pytorch:r32.4.4-pth1.6-py3
+elif [[ "$L4T_VERSION" == "32.7.1" ]]
+then
+	JETBOT_BASE_IMAGE=nvcr.io/nvidia/l4t-pytorch:r32.7.1-pth1.10-py3
 else
 	echo "JETBOT_BASE_IMAGE not found for ${L4T_VERSION}.  Please manually set the JETBOT_BASE_IMAGE environment variable. (ie: export JETBOT_BASE_IMAGE=...)"
 fi
